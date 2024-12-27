cd5c896bcaeced5281f6a5ee8f2c99c1ba900622
Egoroop <elpn14@gmail.com>
2024-12-27 15:47:21 +0300

    Add files via upload

---
+++ cart.js
@@ -0,0 +1,7 @@
+let cart = [];
+
+function addToCart() {
+    const productName = "Грунтовый каток LiuGong (Люгонг) CLG 6616E";
+    cart.push(productName);
+    alert(`${productName} добавлен в корзину!`);
+}
